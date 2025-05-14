c54b285153f453688a727a503007ab2f88019bde
unknown <ashley.chen@jatango.com>
2025-05-14 11:20:18 -0400

    change task background to blue

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -41,7 +41,7 @@ export default function Task({
           name="title"
           id={`title-${id}`}
           placeholder="Input title"
-         style={{ backgroundColor: 'red' }}
+         style={{ backgroundColor: 'blue' }}
         />
       </label>
       {state !== "TASK_ARCHIVED" && (
